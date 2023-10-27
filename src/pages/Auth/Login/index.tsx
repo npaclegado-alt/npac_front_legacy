d610dcb8394d02c26020febb929db818bd84939e
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2023-10-27 10:36:13 -0300

    implementando pagina de login

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React from "react"; 
+import  './styles/login.scss'
 
-const Login: React.FC = ()=> {
+
+function Login():  JSX.Element  {
   return (
-   <div>login</div>
+   <section className={"page-login"}>
+       <nav className="page-login-navegation">
+          <div className="page-login-navegation-logo">
+            <h4>Plano<span>NPAC</span></h4>
+          </div> 
+
+          <ul className="page-login-navegation-list-menu">
+            <li>O que é</li> 
+            <li>Que plano</li>
+            <li>Quem somos</li>           
+          </ul>
+       </nav> 
+
+       <div className="page-login-registration-form">
+         <form>
+             <h2>Faça seu login abaixo</h2>
+            <div className="page-login-registration-form-input">
+               <input type="text" placeholder="Usuário ou Email" />
+            </div> 
+            <div className="page-login-registration-form-input">
+               <input type="password" placeholder="Sua senha"/>
+            </div> 
+            <button type="submit">Fazer Login</button> 
+
+            <p>Sou novo por aqui. <span>Encontrar parceiro para me auxiliar!</span></p>
+         </form>
+       </div>
+   </section>
   );
 }
 
