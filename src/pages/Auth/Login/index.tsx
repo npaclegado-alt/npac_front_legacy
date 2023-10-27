d737e6b8098c42aa675127b5037ab2c847b16d94
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2023-10-27 19:40:53 -0300

    implementando tela de login e integração

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,37 +1,80 @@
-import React from "react"; 
-import  './styles/login.scss'
+import React, { ChangeEvent, useContext, useState } from "react";
+import styles from './styles/login.module.scss'
+import { User, Lock, Eye, EyeOff } from 'lucide-react';
+import { Link } from "react-router-dom";
+import { ContextApi } from "../../../contexts"
+
+type userType = {
+  email: string;
+  password: string
+}
 
 
-function Login():  JSX.Element  {
+function Login(): JSX.Element {
+  
+ 
+ const {loginRequest} = useContext(ContextApi)
+
+  const [lookPassword, setLookPassword] = useState(false)
+  const [user, setUser] = useState<userType>({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setUser({ ...user, [e.target.name]: e.target.value })
+
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    loginRequest(user.email, user.password)
+  } 
+
   return (
-   <section className={"page-login"}>
-       <nav className="page-login-navegation">
-          <div className="page-login-navegation-logo">
-            <h4>Plano<span>NPAC</span></h4>
-          </div> 
+    <section className={styles.pageLogin}>
+      <nav className={styles.pageLoginNavegation}>
+        <div className={styles.pageLoginNavegationLogo}>
+          <h4>Plano<span>NPAC</span></h4>
+        </div>
 
-          <ul className="page-login-navegation-list-menu">
-            <li>O que é</li> 
-            <li>Que plano</li>
-            <li>Quem somos</li>           
-          </ul>
-       </nav> 
+        <ul className={styles.pageLoginNavegationListMenu}>
+          <li>
+            <Link to="#">O que é</Link>
+          </li>
+          <li>
+            <Link to="#">Que plano</Link>
+          </li>
+          <li>
+            <Link to="#">
+              Quem somos
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-       <div className="page-login-registration-form">
-         <form>
-             <h2>Faça seu login abaixo</h2>
-            <div className="page-login-registration-form-input">
-               <input type="text" placeholder="Usuário ou Email" />
-            </div> 
-            <div className="page-login-registration-form-input">
-               <input type="password" placeholder="Sua senha"/>
-            </div> 
-            <button type="submit">Fazer Login</button> 
+      <div className={styles.pageLoginRegistrationForm}>
+        <form onSubmit={handleSubmit}>
+          <h2>Faça seu login abaixo</h2>
+          <div className={styles.pageLoginRegistrationFormInput}>
+            <User className={styles.pageLoginRegistrationInputIcon} size={20} color="#AEAEAE" />
+            <input type="text" placeholder="Usuário ou Email" onChange={handleChange} value={user.email}  name="email"/>
+          </div>
+          <div className={styles.pageLoginRegistrationFormInput}>
+            <Lock className={styles.pageLoginRegistrationInputIcon} size={20} color="#AEAEAE" />
+            <input type={lookPassword ? 'text' : 'password'} placeholder="Sua senha" onChange={handleChange} value={user.password} name="password" />
 
-            <p>Sou novo por aqui. <span>Encontrar parceiro para me auxiliar!</span></p>
-         </form>
-       </div>
-   </section>
+            <button type="button"
+              onClick={() => setLookPassword(look => !look)}
+            >
+              {
+                lookPassword ? <EyeOff size={20} color="#AEAEAE" /> : <Eye size={20} color="#AEAEAE" />
+              }
+            </button>
+          </div>
+          <button type="submit">Fazer Login</button>
+
+          <p>Sou novo por aqui. <span>Encontrar parceiro para me auxiliar!</span></p>
+        </form>
+      </div>
+    </section>
   );
 }
 
