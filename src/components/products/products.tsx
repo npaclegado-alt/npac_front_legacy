662f86ba401488a6a8c98c4cbec46f98319e7a4c
Leandro Goncalves <leandrodebritogoncalves@gmail.com>
2023-10-26 22:04:33 -0400

    Card product

--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,9 +1,47 @@
+import { Files } from 'lucide-react';
+
+import { Divider } from '../divider';
+
+import TenisImage from '../../assets/images/tenisStore.jpg';
+
 import styles from './styleProduct.module.scss';
 
-export function Products(): JSX.Element {
+interface ProductsProps {
+    key: number;
+    name: string;
+    value: number;
+    auffs: number;
+    image: string;
+    link: string;
+}
+export function Products({
+    key,
+    name,
+    value,
+    auffs,
+    image,
+    link
+}: ProductsProps): JSX.Element {
     return (
-        <div className={styles.contentProduct}>
-            <h1>this is may product</h1>
+        <div 
+            className={styles.contentProduct}
+            key={key}
+        >
+            <div className={styles.boxHeaderProduct}>
+                <h4>{name}</h4>
+                <div className={styles.boxValue}>
+                    <p>R$19,90</p>
+                    <p>100 Auffs</p>
+                </div>
+            </div>
+            <div className={styles.boxImage}>
+                <img src={TenisImage} alt="Tenis Nike" />
+            </div>
+            <Divider />
+            <div className={styles.boxLink}>
+                <Files />
+                <p>Link da oferta</p>
+            </div>
         </div>
     );
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,8 +2,84 @@ import React from "react";
 import { Products } from "../../components/products/products";
 
 const Dashboard: React.FC = ()=> {
+  const mockProducts = [
+    {
+      id: 1,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 2,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 3,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 4,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 5,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 6,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 7,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+    {
+      id: 8,
+      name: "Tenis Nike",
+      value: 19.9,
+      auffs: 100,
+      image: "https://imgnike-a.akamaihd.net/1920x1920/015883IF.jpg",
+      link: "https://www.nike.com.br/Produto/Tenis-Nike-Revolution-5-Masculino/111-169-001-169001?site_id=106"
+    },
+  ];
+
   return (
-   <Products />
+    mockProducts.map((product) => (
+      <Products
+        key={product.id}
+        name={product.name}
+        value={product.value}
+        auffs={product.auffs}
+        image={product.image}
+        link={product.link}
+      />
+    ))
   );
 }
 
