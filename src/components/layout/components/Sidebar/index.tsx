953f4c9fa1cbf176f65c2c49857bdcec121c2e39
Guilherme Martins <guilhermepzm@outlook.com>
2023-10-29 14:06:19 -0300

    fixado tamanhos

--- src/components/layout/components/Sidebar/index.tsx
+++ src/components/layout/components/Sidebar/index.tsx
@@ -1,9 +1,23 @@
+import { Divider } from "../../../divider";
 import styles from "./styleSidebar.module.scss";
 
 export default function Sidebar() {
   return (
-    <aside aria-label="Sidenav" className={styles.sidebar}>
-      <div>sidebar</div>
+    <aside className={styles.sidebar}>
+      <div className={styles.container}>
+        <div>Graduação</div>
+        <div>foto perfil</div>
+        <div>Davi Carlos Rodrigues</div>
+        <div>#102030AZ</div>
+        <div>Ativo</div>
+        <Divider />
+        <div>Visão Geral</div>
+        <div>Minha Loja</div>
+        <div>Estrutura</div>
+        <div>Carreira</div>
+        <div>Financeiro</div>
+        <div>Ajuda</div>
+      </div>
     </aside>
   );
 }
