d303d7b17799abc200ffb7fd1bb277dde9a3bf05
Guilherme Martins <guilhermepzm@outlook.com>
2023-10-29 16:14:43 -0300

    login ok

--- src/routes/PrivateWrapper.tsx
+++ src/routes/PrivateWrapper.tsx
@@ -1,17 +1,21 @@
-
-import React, { useContext } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { ContextApi } from '../contexts'
+import React, { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { ContextApi } from "../contexts";
+import api from "../services/api";
 
 const PrivateWrapper: React.FC = () => {
+  const { isAuthenticated, user } = useContext(ContextApi);
 
-  const {isAuthenticated} = useContext(ContextApi)
-  
   if (!isAuthenticated) {
-    return <Navigate to="/login" />
+    return <Navigate to="/login" />;
   }
 
-  return <Outlet />
-}
+  if (user) {
+    //TODO expiração de token
+    api.defaults.headers.Authorization = `Bearer ${user.token}`;
+  }
 
-export default PrivateWrapper
+  return <Outlet />;
+};
+
+export default PrivateWrapper;
