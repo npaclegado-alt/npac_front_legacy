3079d4b1a16c9c5dcd269c50a3e452045f595d8f
gabrielwansoft <gabriel.sousa@wansoft.com.br>
2023-11-01 09:43:15 -0300

    implementando tela de dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from './dashboard.module.scss'
 
 const Dashboard: React.FC = () => {
-  return <div></div>;
+  return (
+    <section className={styles.deshBoardPage}>
+      <div className={styles.deshBoardPageAuff}>
+         <h2>Auffs Gerados</h2>
+        <div>
+        </div> 
+        <div>2</div>
+      </div>
+      <div className={styles.deshBoardPageTotals}>
+        2
+      </div> 
+
+      <div className={styles.deshBoardPageImage}>
+        2
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
