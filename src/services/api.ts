6e5333940fa33f8d56c3513a2bf2e8568b3b77f3
Leandro Goncalves <leandrodebritogoncalves@gmail.com>
2023-12-21 16:07:53 -0400

    switch API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { API_URL } from "../constants/env";
 
 const api = axios.create({
-  //baseURL: API_URL,
-  baseURL: 'http://localhost:4001',
+  baseURL: API_URL,
+  //baseURL: 'http://localhost:4001',
 });
 
 export default api;
